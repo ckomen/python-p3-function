ebbad41653587469b640b37ccd74b6ea6d941f55
komen <cjepkorir09@gmail.com>
2025-11-01 21:41:49 +0300

    Initial commit

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,35 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print("Hello, programmer!")
     pass
 
 def greet(name):
+    print(f"Hello, {name}!")
     pass
 
 def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
     pass
 
 def add(num1, num2):
-    pass
+    return num1+num2
+
+sum_result=add(1+2)
+print(sum_result)
+
+
+pass
 
 def halve(number):
+    if isinstance (number (int , float)): 
+        return number/ 2
+    else:
+        return None
+    
+    result1 =halve(4)
+    print(result1)
+
+    result2 = halve("two")
+    print(result2)
     pass
